c158493dc19ec857b3821a340045329ff2f837e5
Danny <danny.mhlv@gmail.com>
2023-04-11 11:44:29 +0300

    Updated test-executable

--- test_exec/main.c
+++ test_exec/main.c
@@ -1,36 +1,83 @@
-//
-// Created by danny on 13.01.23.
-//
+#include "../wrapper/dh-wrap.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-#include "../library/library.h"
+int main(int argc, char* argv[]) {
+    if (argc < 5) {
+        printf("Not enough arguments to connect!\n");
+        return 11;
+    }
 
-int main() {
-    if (!SDK_init()) {
+    if (!DH_SDK_Init(NULL, 0)) {
         printf("Error initializing SDK!\n");
         return 1;
     }
 
-    long login_id = connect_param("admin", "12345678a", "192.168.1.250", 37777);
+    /**
+     * Login into DVR/NVR
+     */
+    struct login_info info = {};
+    strncpy(info.ip, argv[1], 64);
+    info.port = atoi(argv[2]);
+    strncpy(info.username, argv[3], 64);
+    strncpy(info.password, argv[4], 64);
+
+    long login_id = DH_Login(&info);
     if (!login_id) {
-        printf("%x", get_error());
+        printf("%x", DH_GetLastError());
         return 2;
     }
 
-    buffer buff;
-    init_buff(&buff, 2 * 1024 * 1024);
-    get_data(login_id,
-             BOTH,
-             &(dtstamp){ .year = 2023, .month = 1, .day = 17, .hour = 12, .min = 42, .sec = 0 },
-             &(dtstamp){ .year = 2023, .month = 1, .day = 17, .hour = 12, .min = 43, .sec = 0 },
-             &buff);
+    struct dh_getcam_out* output = NULL;
+    output = DH_GetCameras(login_id, 30);
 
-    disconnect(login_id);
+    if (!output) {
+        return 3;
+    }
 
-    FILE* file = fopen("./myvid.dav", "wb");
-    fwrite(buff.data, sizeof(byte), buff.size, file);
-    fclose(file);
+    /**
+     * List all received cameras
+     */
+    if (output) {
+        for (int i = 0; i < output->totalCamsReturned; i++) {
+            printf("IP: %s\n", output->cameras[i].remoteDevInfo.ip);
+            printf("Camera name: %s\n", output->cameras[i].remoteDevInfo.name);
+            printf("Class: %s\n", output->cameras[i].remoteDevInfo.devClass);
+            printf("Type: %s\n", output->cameras[i].remoteDevInfo.devType);
+            printf("Type: %d\n", output->cameras[i].vidStreamType);
+            printf("--------------------------------------------------------\n");
+        }
+    }
 
+    /**
+     * Login into camera
+     */
+    struct login_info camera_info = {};
+    strncpy(camera_info.ip, output->cameras[0].remoteDevInfo.ip, 64);
+    camera_info.port = output->cameras[0].remoteDevInfo.port;
+    strncpy(camera_info.username, output->cameras[0].remoteDevInfo.user, 64);
+    strncpy(camera_info.password, argv[4], 64);
+    if (strlen(output->cameras[0].remoteDevInfo.pass)) {
+        strncpy(camera_info.password, output->cameras[0].remoteDevInfo.pass, 64);
+    }
 
-    SDK_cleanup();
+    long cam_login_id = DH_Login(&camera_info);
+    if (!cam_login_id) {
+        printf("Error: %x\n", DH_GetLastError());
+        return 2;
+    }
+
+    /**
+     * Get device config
+     */
+    char* x = (char*)calloc(1024*100, sizeof(char));
+    DH_GetConfigJSON(login_id, CFG_CMD_RTSP, -1, x, 1024 * 100 * sizeof(char));
+
+    struct config_rtsp data = {};
+    data.size = sizeof(struct config_rtsp);
+    DH_ParseConfigJSON(CFG_CMD_RTSP, x, &data, data.size);
+
+    DH_SDK_Cleanup();
     return 0;
 }
