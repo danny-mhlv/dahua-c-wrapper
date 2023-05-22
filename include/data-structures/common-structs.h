a809a407bc5f192eeed37fd158f50af477f63941
Danny <danny.mhlv@gmail.com>
2023-05-22 13:58:49 +0300

    Added Find file functionality.
    Now it is possible to find multiple files with a specified expanded query.

--- include/data-structures/common-structs.h
+++ include/data-structures/common-structs.h
@@ -5,6 +5,9 @@
 extern "C" {
 #endif
 
+#include "../constants/common-constants.h"
+#include "../enums.h"
+
 /**
  * Represents date-time point
  * @dahua_name NET_TIME
@@ -26,6 +29,34 @@ struct login_info {
     int port;
     char username[64];
     char password[64];
+};
+
+///@brief card no. record info
+
+/**
+ * Card info
+ * @dahua_name NET_RECORD_CARD_INFO
+ * @associated_structure find_file_in
+ * @param size - Struct size
+ * @param type - Type. Card (0), field (1)
+ * @param cardNumber - Card number
+ * @param tradeType - Transaction type
+ * @param amount - Transaction amount. If string is null - means no-limit amount
+ * @param error - Error code. All errors (0), retain cash (1), retain card (2)
+ * @param fieldCount - ?
+ * @param fields - ?
+ * @param change - Change
+ */
+struct card_info {
+    unsigned int                size;
+    int                         type;
+    char                        cardNumber[DH_MAX_CARD_INFO_LEN];
+    enum atm_transaction_type   tradeType;
+    char                        amount[64];
+    int                         error;
+    int                         fieldCount;    				// domain quantity, by domain search is valid
+    char                        fields[MAX_CARD_RECORD_FIELD_NUM][256];   // domain info, by domain search is valid
+    char				        change[32];
 };
 
 #ifdef __cplusplus
--- include/data-structures/video-structs.h
+++ include/data-structures/video-structs.h
@@ -2,6 +2,8 @@
 #define VIDEO_STRUCTS_H
 
 #include "common-structs.h"
+#include "event-structs.h"
+
 #include "../constants/common-constants.h"
 
 #ifdef __cplusplus
@@ -39,7 +41,7 @@ struct video_frame_info {
  * @dahua_name tagNET_IN_QUERY_RECORD_FILE_EX
  * @associated_method DH_QueryRecordFiles(...)
  * @param size - Struct size
- * @param channel - Channel ID
+ * @param channel - Channel ID. <i>Note: starts from 0</i>.
  * @param recordFileType - Record file type
  * @param startTime - Start time
  * @param endTime - End time
@@ -57,10 +59,10 @@ struct query_record_file_in {
     struct time_stamp		startTime;
     struct time_stamp		endTime;
     char*					cardId;
-    int						queryByTime;                  // Whether to query by time
-    int						useRealUTC;	// TRUE means only deliver stuStartTimeRealUTC and stuEndTimeRealUTC (do not deliver stuStartTime, stuEndTime), FALSE means deliver only stuStartTime, stuEndTime (do not deliver stuStartTimeRealUTC and stuEndTimeRealUTC)
-    struct time_stamp		startTimeRealUTC;	// UTC start time (standard UTC time), paired with stuEndTimeRealUTC, mutually exclusive with (stuStartTime, stuEndTime)
-    struct time_stamp		endTimeRealUTC;		// UTC end time (standard UTC time), paired with stuStartTimeRealUTC, mutually exclusive with (stuStartTime, stuEndTime)
+    int						queryByTime;
+    int						useRealUTC;
+    struct time_stamp		startTimeRealUTC;
+    struct time_stamp		endTimeRealUTC;
 };
 
 /**
@@ -114,6 +116,7 @@ struct record_info {
 
 /**
  * @dahua_name tagNET_RECORDFILE_INFOEX
+ * @associated_structure query_record_file_out
  * @param recordFileInfo - Record file information
  * @param eventList - Associated event list [IVS events]
  * @param eventCount - Number of received events
@@ -134,6 +137,147 @@ struct extended_record_info {
     struct time_stamp			startTimeRealUTC;
     struct time_stamp			endTimeRealUTC;
     unsigned char               RESERVED[452];
+};
+
+/**
+ * Input parameter to be filled to find files
+ * @dahua_name tagNET_IN_MEDIA_QUERY_FILE
+ * @associated_method DH_FindFiles(...)
+ * @param size - Struct size
+ * @param directories - Working directory list.
+ * Directories are separated by ";". Example "/mnt/dvr/sda0;/mnt/dvr/sda1".
+ * If <b>directories</b> is null or an empty string - SDK will perform the search in all directories.
+ * @param mediaType - Media type to search for. Any type (0), JPG (1), DAV(2)
+ * @param channel - Channel ID. <i>Note: starts from 0. Value of -1 forces SDK to search all channels</i>.
+ * @param startTime - Start time
+ * @param endTime - End time
+ * @param eventList - List of event types to search with (Intelligent analysis event [IVS])
+ * @param eventNum - Number of events
+ * @param videoStream - Video stream type. Unknown (0), main (1),
+ * sub-stream 1 (2), sub-stream 2 (3), sub-stream 3 (4), ExtraX (5)
+ * @param flagList - Flag list to search with
+ * @param flagNum - Number of flags
+ * @param cardInfo - ?
+ * @param userNum - Number of users
+ * @param userNames - ?
+ * @param order - Result order
+ * @param findByTime - Flag that indicates whether to search by time or not
+ * @param combine - Flag that indicates whether to combine found files or not
+ * @param eventInfo - ?
+ * @param eventInfoNum -Number of events
+ * @param findByRealUTC - Flag to skip timestamps <b>startTime</b> and <b>endTime</b>,
+ * and use <b>startTimeRealUTC</b> and <b>endTimeRealUTC</b>
+ * @param startTimeRealUTC - Start time
+ * @param endTimeRealUTC - End time
+ */
+struct find_file_in {
+    unsigned int              		size;
+    char*               			directories;
+    int								mediaType;
+    int                 			channel;
+    struct time_stamp            	startTime;
+    struct time_stamp            	endTime;
+    int                 			eventList[MAX_IVS_EVENT_NUM];
+    int                 			eventNum;
+    unsigned char                	videoStream;
+    unsigned char               	RESERVED[3];
+    enum record_metamark_type		flagList[METAMARK_TYPE_MAX];
+    int                 			flagNum;
+    struct card_info 			    cardInfo; // card no. record info, emFalgLists including card no. video is valid
+    int                 			userNum;             // user total
+    char                			userNames[MAX_QUERY_USER_NUM][DH_NEW_USER_NAME_LENGTH]; // user name
+    enum result_order 			    order;
+    int                			    findByTime;
+    enum file_combination_mode		combine;
+    struct event_info				eventInfo[16];		// event info(customized),when query type in EM_FILE_QUERY_TYPE is DH_FILE_QUERY_FILE_EX valid
+    int								eventInfoNum;
+    int							    findByRealUTC;
+    struct time_stamp				startTimeRealUTC;
+    struct time_stamp				endTimeRealUTC;
+};
+
+///@brief record info, corresponde to CLIENT_FindFileEx, search result
+/**
+ * @dahua_name NET_FILE_SUMMARY_INFO
+ * @associated_structure find_file_out
+ * @param key - Key
+ * @param value - Value
+ */
+struct file_summary {
+    char key[64];
+    char value[512];
+    unsigned char RESERVED[256];
+};
+
+/**
+ * Output parameter to be filled by SDK when finding files
+ * @dahua_name NET_OUT_MEDIA_QUERY_FILE
+ * @associated_method DH_FindFiles(...)
+ * @param size - Struct size
+ * @param channel - Channel ID
+ * @param startTime - Start time
+ * @param endTime - End time
+ * @param fileType - File type. JPG (1), DAV(2)
+ * @param partition - Partition number
+ * @param videoStream - Video stream type. Unknown (0), main (1),
+ * sub-stream 1 (2), sub-stream 2 (3), sub-stream 3 (4), ExtraX (5)
+ * @param cluster - Cluster
+ * @param filePath - File path
+ * @param eventList - List of event types received (Intelligent analysis event [IVS])
+ * @param eventNum - Number of events received
+ * @param flagList - Flag list received
+ * @param flagNum - Number of flags received
+ * @param driveNumber - Drive number
+ * @param synopsisPicPath - Synopsis snapshot file path ?
+ * @param synopsisMaxTime - Video synopsis maximum time in seconds ?
+ * @param synopsisMinTime - Video synopsis minimum time in seconds ?
+ * @param fileSummaryNum - File summary number
+ * @param fileSummary - File summary list
+ * @param fileSize - File size in bytes
+ * @param frameNum - Number of frames
+ * @param fileState - File state. Unknown (0), temporary (1), complete (2)
+ * @param workDir - Storage directory of video files
+ * @param thumbnailPath - Thumbnail path
+ * @param useRealUTC - Flag to skip timestamps <b>startTime</b> and <b>endTime</b>,
+ * and use <b>startTimeRealUTC</b> and <b>endTimeRealUTC</b>
+ * @param startTimeRealUTC - Start time
+ * @param endTimeRealUTC - End time
+ * @deprecated
+ * nFileSize - File size. Should not be used, marked as deprecated by SDK.
+ * Use <b>fileSize</b> instead.\n\n
+ * byDriveNo - Drive number. Should not be used, marked as deprecated by SDK.
+ * Use <b>driveNumber</b> instead.\n\n
+ */
+struct find_file_out {
+    unsigned int                size;
+    int					        channel;
+    struct time_stamp           startTime;
+    struct time_stamp	        endTime;
+    unsigned int		        nFileSize;
+    unsigned char               fileType;
+    unsigned char               byDriveNo;
+    unsigned char               partition;
+    unsigned char               videoStream;
+    unsigned int                cluster;
+    char				        filePath[MAX_PATH];
+    int                         eventList[MAX_IVS_EVENT_NUM];
+    int                         eventNum;
+    enum record_metamark_type   flagList[METAMARK_TYPE_MAX];
+    int                         flagNum;
+    unsigned int                driveNumber;
+    char                        synopsisPicPath[512];
+    int                         synopsisMaxTime;
+    int                         synopsisMinTime;
+    int                         fileSummaryNum;
+    struct file_summary         fileSummary[MAX_FILE_SUMMARY_NUM];
+    long long                   fileSize;
+    unsigned int			    frameNum;
+    enum video_file_state       fileState;
+    char                        workDir[256];
+    char					    thumbnailPath[260];
+    int					        useRealUTC;
+    struct time_stamp           startTimeRealUTC;
+    struct time_stamp           endTimeRealUTC;
 };
 
 #ifdef __cplusplus
